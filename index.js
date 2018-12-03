06e42815f4f8578ff4eaaf8f30f3563656eeb05f
Charnell Dolley <cdolley516@gmail.com>
2018-12-03 16:04:02 -0500

    adding routes

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
-"use strict"; {
-    angular.module('app', []);
+"use strict";
+{
+    angular.module('app',['ngRoute'])
+        .config( function($routeProvider){
+            $routeProvider
+                .when('/',{
+                    template: '<home></home>'
+                })
+                .when('/game',{
+                    template: '<game></game>'
+                })
+                .when('/score',{
+                    template: '<score></score>'
+                })
+                .otherwise({
+                    template: '<h1>404 PAGE DOES NOT EXIST</h1>'
+                })
+        });
 }
 
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
 
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+// "use strict"; {
+//     angular.module('app', []);
+// }
 
-    let dropdowns = document.getElementsByClassName("dropdown-content");
-    let i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
+// function myFunction() {
+//     document.getElementById("myDropdown").classList.toggle("show");
+// }
+
+// window.onclick = function(event) {
+//   if (!event.target.matches('.dropbtn')) {
+
+//     let dropdowns = document.getElementsByClassName("dropdown-content");
+//     let i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+// }

8165253c1ba17b124fa06c8075a3d34bf2b1e574
dfili8 <dfilimon.s@gmail.com>
2018-12-03 19:21:12 -0500

    api token request working

---
+++ proxy-server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const token = require('./token-router');
+const app = express();
+const port = 8080;
+
+app.use(express.json());
+app.use('/', express.static('./public'));
+app.use('/access-token', token);
+
+app.listen(port, function(){
+    console.log(`Server up and running on localhost:${port}`);
+});

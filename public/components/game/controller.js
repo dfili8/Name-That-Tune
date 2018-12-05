4cb7ea77d73e3656fb186a0db18358d7ec661f57
dfili8 <dfilimon.s@gmail.com>
2018-12-04 20:00:47 -0500

    GET and POST method to recieve data.

--- public/components/game/controller.js
+++ public/components/game/controller.js
@@ -1,9 +1,23 @@
 {
     angular.module('app')
-        .controller('GameController', function(){
+        .controller('GameController', function($http){
             const $ctrl = this;
-            
-            
-        
+
+            $http.post('/access-token').then(function(response){
+                $ctrl.tokenResponse = response.data;
+                $ctrl.myToken = $ctrl.tokenResponse.access_token;
+            });
+
+            $http({
+                method: 'GET',
+                url: 'https://api.spotify.com/v1/tracks?ids=6rqhFgbbKwnb9MLmUQDhG6',
+                data: {
+                    Authorization: `Bearer ${$ctrl.myToken}`
+                }
+            }).then(function(response){
+                console.log(response.data);
+                $ctrl.tracks = response.data;
+                console.log($ctrl.tracks);
+            })
     });
 };

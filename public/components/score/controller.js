cd47660b7a06a0dcaa545aa74cff2fe73c9c892a
Charnell Dolley <cdolley516@gmail.com>
2018-12-10 20:59:57 -0500

    adding tablet css and score changes

--- public/components/score/controller.js
+++ public/components/score/controller.js
@@ -7,10 +7,6 @@
 
         
 
-<<<<<<< HEAD
-            // const score  = GameService.score;
-=======
->>>>>>> be924a836610109b0ba210ea778c24de385ec72a
             $ctrl.playerScore = GameService.score;
 
         

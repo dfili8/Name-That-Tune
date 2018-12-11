897271c1dc7c4735c782dc3facdc0e2f757fb67b
dfili8 <dfilimon.s@gmail.com>
2018-12-10 20:56:13 -0500

    Adding additional categories to app

--- public/components/score/controller.js
+++ public/components/score/controller.js
@@ -5,14 +5,6 @@
             
             $ctrl.playerName = GameService.name;
 
-        
-
-<<<<<<< HEAD
-            // const score  = GameService.score;
-=======
->>>>>>> be924a836610109b0ba210ea778c24de385ec72a
             $ctrl.playerScore = GameService.score;
-
-        
     });
 };

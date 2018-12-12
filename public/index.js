64448e444f65286b88434835b6b572cc2cec9207
Charnell Dolley <cdolley516@gmail.com>
2018-12-12 17:23:02 -0500

    fixing location

--- public/index.js
+++ public/index.js
@@ -18,25 +18,3 @@
         });
 }
 
-
-// "use strict"; {
-//     angular.module('app', []);
-// }
-
-// function myFunction() {
-//     document.getElementById("myDropdown").classList.toggle("show");
-// }
-
-// window.onclick = function(event) {
-//   if (!event.target.matches('.dropbtn')) {
-
-//     let dropdowns = document.getElementsByClassName("dropdown-content");
-//     let i;
-//     for (i = 0; i < dropdowns.length; i++) {
-//       var openDropdown = dropdowns[i];
-//       if (openDropdown.classList.contains('show')) {
-//         openDropdown.classList.remove('show');
-//       }
-//     }
-//   }
-// }
